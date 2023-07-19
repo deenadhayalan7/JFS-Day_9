73cfac9856cae5058abb91f315ffa45cbf31f9aa
deenadhayalan7 <113279582+deenadhayalan7@users.noreply.github.com>
2023-07-19 21:15:38 +0530

    Add files via upload

---
+++ src/main/java/com/example/demo/Repos/UserRepo.java
@@ -0,0 +1,11 @@
+package com.example.demo.Repos;
+
+
+
+import org.springframework.data.jpa.repository.JpaRepository;
+
+import com.example.demo.Models.User;
+
+public interface UserRepo extends JpaRepository<User, Long> {
+
+}
